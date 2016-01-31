03589948553d4a078b28edf0533692390602b982
Thierry Chatel <tchatel@gmail.com>
2016-01-31 18:32:18 +0100

    step #4

--- src/travel-edit.ts
+++ src/travel-edit.ts
@@ -1,22 +1,22 @@
 /*
- <h2>Edit travel: ****</h2>
- <form class="pure-form pure-form-aligned">
- <fieldset>
- <div class="pure-control-group">
- <label for="name">Place</label>
- <input id="name" type="text"/>
- </div>
- <div class="pure-control-group">
- <label for="email">Country</label>
- <input id="email" type="text"/>
- </div>
- <div class="pure-control-group">
- <label for="phone">Year</label>
- <input id="phone" type="number"/>
- </div>
- <div class="pure-controls">
- <a class="pure-button pure-button-primary">Return to list</a>
- </div>
- </fieldset>
- </form>
- */
+<h2>Edit travel: ****</h2>
+<form class="pure-form pure-form-aligned">
+    <fieldset>
+        <div class="pure-control-group">
+            <label for="name">Place</label>
+            <input id="name" type="text"/>
+        </div>
+        <div class="pure-control-group">
+            <label for="email">Country</label>
+            <input id="email" type="text"/>
+        </div>
+        <div class="pure-control-group">
+            <label for="phone">Year</label>
+            <input id="phone" type="number"/>
+        </div>
+        <div class="pure-controls">
+            <a class="pure-button pure-button-primary">Return to list</a>
+            </div>
+    </fieldset>
+</form>
+*/

1fa0f8b37421d4c319eed6d2d2e18c040f030c8a
Thierry Chatel <tchatel@gmail.com>
2016-01-31 18:45:14 +0100

    step #3

--- src/boot-travel.ts
+++ src/boot-travel.ts
@@ -1,9 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {provide} from "angular2/core";
-import {LocationStrategy} from "angular2/router";
-import {HashLocationStrategy} from "angular2/router";
-import {ROUTER_PROVIDERS} from "angular2/router";
-import {APP_BASE_HREF} from "angular2/router";
 
 import {TravelApp} from "./travel-app";
 import {TravelManager} from "./travel-manager";
@@ -11,8 +7,5 @@ import {SampleTravelManager} from "./travel-manager";
 
 
 bootstrap(TravelApp, [
-    provide(TravelManager, {useClass: SampleTravelManager}),
-    ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass: HashLocationStrategy}),
-    provide(APP_BASE_HREF, {useValue: '/'})
+    provide(TravelManager, {useClass: SampleTravelManager})
 ]);
